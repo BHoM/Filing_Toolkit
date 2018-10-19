6f994bdac853d7bf41ab7cab9dc9382e1a79b4ad
Andrew Wakeman <andrew.wakeman@burohappold.com>
2018-10-19 10:11:03 +0100

    Create initial boilerplate
    For Filing_Engine and Filing_Adapter.

---
+++ Filing_Adapter/CRUD/Read.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Filing
+{
+    public partial class FilingAdapter
+    {
+        protected override IEnumerable<IBHoMObject> Read(Type type, IList ids)
+        {
+            return new List<BHoMObject>();
+        }
+    }
+}
---
+++ Filing_Engine/Convert/ToBHoM.cs
@@ -0,0 +1,24 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Filing
+{
+    public static partial class Convert 
+    {
+        public static IBHoMObject ToBHoM(this FileInfo file)
+        {
+            return new BHoMObject() { Name = file.Name };
+        }
+
+        public static IBHoMObject ToBHoM(this DirectoryInfo dir)
+        {
+            return new BHoMObject() { Name = dir.Name };
+        }
+
+    }
+}
