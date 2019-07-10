097dec7f2e0e5ab49468e6f05253b7fcb0aa4a4c
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-07-10 10:17:42 +0100

    Bump Version to 2.4.0.0

--- Filing_Test/Properties/AssemblyInfo.cs
+++ Filing_Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("36521f16-d556-4f90-a427-724034f05333")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
