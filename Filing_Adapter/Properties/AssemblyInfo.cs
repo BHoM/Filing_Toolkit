c5938c38a2858dc4a6786b9246c079dd910f765a
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-11-07 10:55:25 +0000

    Update Assembly copyright

--- Filing_Adapter/Properties/AssemblyInfo.cs
+++ Filing_Adapter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BuroHappold")]
 [assembly: AssemblyProduct("Filing_Adapter")]
-[assembly: AssemblyCopyright("Copyright © BuroHappold 2018")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
