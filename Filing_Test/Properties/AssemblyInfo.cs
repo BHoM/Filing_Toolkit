a9e98b63395db36fb0fedabfc291ef08656efb6a
Andrew Wakeman <andrew.wakeman@burohappold.com>
2020-01-02 11:31:02 +0000

    Bump AssemblyFileVersion to 3.1

--- Filing_Test/Properties/AssemblyInfo.cs
+++ Filing_Test/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
