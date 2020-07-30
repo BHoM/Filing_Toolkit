79774213010334e03ddcb3333b5e73664fc2aab9
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-07-30 11:03:34 +0100

    Added PathCombine function

---
+++ Filing_Engine/Query/PathCombine.cs
@@ -0,0 +1,34 @@
+﻿using BH.oM.Base;
+using BH.oM.Filing;
+using BH.oM.Reflection.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Filing
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /*** Methods                                     ***/
+        /***************************************************/
+
+        [Description("Combines two paths.")]
+        public static string PathCombine(string path1, string path2)
+        {
+            return Path.Combine(path1, path2);
+        }
+
+        /***************************************************/
+
+        [Description("Combines two paths.")]
+        public static string PathCombine(List<string> paths)
+        {
+            return Path.Combine(paths.ToArray());
+        }
+    }
+}
