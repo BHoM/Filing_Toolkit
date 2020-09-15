f40c5207e4ea847932d7bd1d0fbe77dd92e16769
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-09-15 17:17:56 +0100

    Bug fix: `PushConfig.DefaultFilePath` wasn't a property.

--- Filing_oM/Config/PushConfig.cs
+++ Filing_oM/Config/PushConfig.cs
@@ -12,11 +12,17 @@ namespace BH.oM.Adapters.Filing
     public class PushConfig : ActionConfig
     {
         [Description("Default filePath used for pushing objects that are not BHoM `File` or `Directory`.")]
-        public string DefaultFilePath = "C:/temp/Filing_Adapter-objects.json";
+        public string DefaultFilePath { get; set; } = "C:/temp/Filing_Adapter-objects.json";
 
         [Description("When updating a File, set whether the input content should be appended to the existing or overwritten." +
             "\nBy default is `false` (= the content of the File is entirely overwritten).")]
         public bool AppendContent { get; set; } = false;
+
+        [Description("When serialising to JSON, use the Dataset serialization style." +
+            "\nThis serializes the individual objects, and then concatenates the strings separating with a newline." +
+            "\nThe obtained format is not valid JSON. You will need to deserialize each individual line." +
+            "\nThis is the current standard for Datasets.")]
+        public bool UseDatasetSerialization { get; set; } = false;
 
         [Description("Keeps the warnings about Deletion off.")]
         public bool DisableWarnings { get; set; } = false;
